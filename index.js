c9345ae9ad0063a76483b5209a34a9a152452959
smithspencer817 <smithspencer817@gmail.com>
2020-12-03 16:42:22 -0600

    added post route to api

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+let baseballPlayers = [
+    {id: 1, name: "Fernando Tatis Jr."},
+    {id: 2, name: "Mookie Betts"},
+    {id: 3, name: "Ronald Acuna Jr."}
+]
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
 app.get('/api/baseball-players', (req, res) => {
-    res.send(["Fernando Tatis Jr.", "Mookie Betts", "Ronald Acuna Jr."])
+    res.send(baseballPlayers)
+});
+
+app.get('/api/baseball-players/:id', (req, res) => {
+    const player = baseballPlayers.find(p => p.id === parseInt(req.params.id));
+    if (!player) res.status(400).send('The player with that ID was not found');
+    res.send(player);
+});
+
+app.post('/api/baseball-players', (req, res) => {
+    const player = {
+        id: baseballPlayers.length + 1,
+        name: req.body.name
+    };
+    baseballPlayers.push(player)
+    res.send(player);
 });
 
 app.listen(3000, () => {

6ab4377fc6d4dea57e789d75414f5f10ba021aa0
smithspencer817 <smithspencer817@gmail.com>
2020-12-04 16:59:14 -0600

    added new sql files

--- sql/db.sql
+++ sql/db.sql
@@ -9,13 +9,14 @@ CREATE TABLE users (
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     email VARCHAR(50) UNIQUE NOT NULL,
-    img BYTEA
+    img BYTEA DEFAULT NULL
 );
 
 CREATE TABLE teams (
     id BIGSERIAL PRIMARY KEY NOT NULL,
     name VARCHAR(50) NOT NULL,
-    img BYTEA
+    league VARCHAR(50) NOT NULL,
+    img BYTEA DEFAULT NULL
 );
 
 CREATE TABLE players (
@@ -23,18 +24,17 @@ CREATE TABLE players (
     team_id REFERENCES teams (id) ON DELETE SET NULL,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
-    position VARCHAR(20) NOT NULL,
-    img BYTEA
+    position VARCHAR(2) NOT NULL,
+    img BYTEA DEFAULT NULL
 )
 
 CREATE TABLE ballparks (
     id BIGSERIAL PRIMARY KEY NOT NULL,
     home_team_id INT REFERENCES teams (id) ON DELETE CASCADE NOT NULL,
     name VARCHAR(50) NOT NULL,
-    league VARCHAR(6) NOT NULL,
     city VARCHAR(50) NOT NULL,
     state VARCHAR(2) NOT NULL,
-    img BYTEA
+    img BYTEA DEFAULT NULL
 );
 
 CREATE TABLE visits (
@@ -53,7 +53,7 @@ CREATE TABLE pictures (
     visit_id INT REFERENCES visits (id) ON DELETE CASCADE NOT NULL,
     title VARCHAR(50),
     description TEXT,
-    img BYTEA NOT NULL,
+    img BYTEA DEFAULT NULL,
     date_taken DATE,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
 )
@@ -62,5 +62,5 @@ CREATE TABLE autographs (
     id BIGSERIAL PRIMARY KEY NOT NULL,
     user_id INT REFERENCES users (id) ON DELETE CASCADE NOT NULL,
     player_id INT REFERENCES players (id) ON DELETE SET NULL,
-    img BYTEA NOT NULL
+    img BYTEA DEFAULT NULL
 )
